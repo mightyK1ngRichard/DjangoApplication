52565fa580f1f352831c8719448ad96e4995e106
Dmitiy <dimapermyakov55@gmail.com>
2023-05-10 15:43:03 +0300

    Update

--- askme_permyakov/static/js/myscript.js
+++ askme_permyakov/static/js/myscript.js
@@ -1,18 +1,26 @@
 const btnRespond = document.getElementById('respond');
 const respondField = document.getElementById('myElement');
-const cancelRespond = document.getElementById('cancel-respond')
-const btnSendRespond = document.getElementById('send-respond')
+const cancelRespond = document.getElementById('cancel-respond');
+const btnSendRespond = document.getElementById('send-respond');
+const inputField = document.getElementById('my-textarea');
+const blockResponses = document.getElementById('block-responses');
+blockResponses.hidden = false;
 
 btnRespond.addEventListener('click', function () {
+    blockResponses.hidden = true;
     btnRespond.disabled = true;
     respondField.hidden = false;
 })
 
 cancelRespond.addEventListener('click', function () {
+    blockResponses.hidden = false;
+
     respondField.hidden = true;
     btnRespond.disabled = false;
 })
 
 btnSendRespond.addEventListener('click', function () {
+    blockResponses.hidden = false;
     // TODO: сделать SQL INSERT
+    console.log(inputField.value)
 })
